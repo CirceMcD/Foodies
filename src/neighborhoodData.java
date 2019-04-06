0625c96f592f8d697114875a56bf075e9d3ff5a2
Circe McDonald <circemcd@gmail.com>
2019-04-06 13:40:11 -0700

    Adding apache commons math package and starting descriptive statistics.

--- src/neighborhoodData.java
+++ src/neighborhoodData.java
@@ -1,45 +1,22 @@
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class neighborhoodData {
     public static final String XLSX_FILE_PATH = "DataDownload.xls";
 
-    public static void main(String[] args) throws IOException, InvalidFormatException {
-
-        // Creating a Workbook from an Excel file (.xls or .xlsx)
+    public Workbook readWorkbook() throws EncryptedDocumentException, IOException {
         Workbook workbook = WorkbookFactory.create(new File(XLSX_FILE_PATH));
-
-        // Retrieving the number of sheets in the Workbook
-        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
-
-        /*
-           =============================================================
-           Iterating over all the sheets in the workbook
-           =============================================================
-        */
-
-        System.out.println("Retrieving Sheets using Java 8 forEach with lambda");
-        workbook.forEach(sheet -> {
-            System.out.println("=> " + sheet.getSheetName());
-        });
-
-        /*
-           ==================================================================
-           Iterating over all the rows and columns in a Sheet 
-           ==================================================================
-        */
-
-        // Getting the Sheet at index zero
-        Sheet sheet = workbook.getSheetAt(4);
-
+    	return workbook;
+    }
+    
+    public void printSheet(Sheet sheet){
         // Create a DataFormatter to format and get each cell's value as String
         DataFormatter dataFormatter = new DataFormatter();
-
-        System.out.println("\n\nIterating over Rows and Columns using Java 8 forEach with lambda\n");
+       
+        //Print out rows and columns using forEach.
         sheet.forEach(row -> {
             row.forEach(cell -> {
                 String cellValue = dataFormatter.formatCellValue(cell);
@@ -47,9 +24,12 @@ public class neighborhoodData {
             });
             System.out.println();
         });
+    }
 
-        // Closing the workbook
-        workbook.close();
+    public void printSheetNames(Workbook workbook){
+        workbook.forEach(sheet -> {
+            System.out.println("=> " + sheet.getSheetName());
+        });
     }
 }
 
