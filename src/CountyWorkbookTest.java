d8ace7f006cc8f05f31abeb136caaac842fb5fa4
Circe <circemcd@gmail.com>
2019-05-11 20:02:19 -0700

    updated directory

--- src/CountyWorkbookTest.java
+++ src/CountyWorkbookTest.java
@@ -10,13 +10,13 @@ class CountyWorkbookTest {
 	
 	@Test
 	void testReadWorkbook() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		assertEquals(2.0,n1.myWorkbook.getSheet("Sheet1").getRow(1).getCell(3).getNumericCellValue());
 	}
 
 	@Test
 	void testCountyCreator() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Map<String, County> counties = n1.countyCreator();
 		String statskeys = counties.get("101010").stats.keySet().toString();
 		assertEquals("[Data2, LastData, Data1]", statskeys);
@@ -24,7 +24,7 @@ class CountyWorkbookTest {
 	
 	@Test
 	void testCountyCreator2() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Map<String, County> counties = n1.countyCreator();
 		String statskeys = counties.get("123456").stats.keySet().toString();
 		assertEquals("[Data2, LastData, Data1]", statskeys);
@@ -32,7 +32,7 @@ class CountyWorkbookTest {
 
 	@Test
 	void testStateCreator() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Map<String, County> counties = n1.countyCreator();
 		Map<String, State> states = n1.stateCreator(counties);
 		assertEquals(1, states.get("NV").stats.get("Data2").getN());
@@ -40,7 +40,7 @@ class CountyWorkbookTest {
 
 	@Test
 	void testListStates() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Map<String, County> counties = n1.countyCreator();
 		ArrayList<String> listOfStates = n1.listStates(counties);
 		//ArrayList<String> expectedStates = new ArrayList<String> {"NV", "CA"};
@@ -49,28 +49,28 @@ class CountyWorkbookTest {
 
 	@Test
 	void testIsDataSheetFalse() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Boolean expected = false;
 		assertEquals(expected, n1.isDataSheet(n1.myWorkbook.getSheet("NotData")));
 	}
 	
 	@Test
 	void testIsDataSheetTrue() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Boolean expected = true;
 		assertEquals(expected, n1.isDataSheet(n1.myWorkbook.getSheet("Sheet1")));
 	}
 
 	@Test
 	void testListDataSheets() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		String outList = n1.listDataSheets().get(1).getSheetName();
 		assertEquals("Sheet2", outList);
 	}
 	
 	@Test
 	void testExtractHumanReadableVariableNames() throws EncryptedDocumentException, IOException {
-		CountyWorkbook n1 = new CountyWorkbook("county_test.xlsx");
+		CountyWorkbook n1 = new CountyWorkbook("test_data/county_test.xlsx");
 		Map<String, String> variableNames = n1.extractHumanReadableVariableNames();
 		String test = variableNames.get("Data1");
 		assertEquals("I'm #1", test);
